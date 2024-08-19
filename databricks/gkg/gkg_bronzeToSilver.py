5fc5c0290e1a58d1b36b566242d95a098365e5bb
hucodelab <hucodelab>
2024-08-19 04:07:21 +0000

    updated: write files to Azure SQL

--- databricks/gkg/gkg_bronzeToSilver.py
+++ databricks/gkg/gkg_bronzeToSilver.py
@@ -66,19 +66,74 @@ df_sorted = df_grouped.orderBy(col("count").desc())
 
 # COMMAND ----------
 
+spark.conf.set("credentials", "/Workspace/Shared/factored-datathon-gold-46ad9a4661b2.json")
+
+# COMMAND ----------
+
+# %fs pwd
+
+# COMMAND ----------
+
+dbutils.fs.ls("dbfs:/FileStore/shared_uploads/hvallejo.77@gmail.com/")
+
+# COMMAND ----------
+
+# Set the necessary configurations
+spark.conf.set("credentialsFile", "dbfs:/FileStore/shared_uploads/hvallejo.77@gmail.com/factored_datathon_gold_46ad9a4661b2.json")
+spark.conf.set("parentProject", "factored-datathon-gold")
+
+# Define the necessary variables
+project_id = "factored-datathon-gold"
+dataset_id = "gkg"
+table_id = "themes"
+
+# Full table name
+table_name = f"{project_id}:{dataset_id}.{table_id}"
+
+# Write DataFrame to BigQuery
+df_sorted.limit(5).write \
+  .format("bigquery") \
+  .option("table", table_name) \
+  .option("parentProject", project_id) \
+  .option("credentialsFile", "dbfs:/FileStore/shared_uploads/hvallejo.77@gmail.com/credentials.json") \
+  .mode("overwrite") \
+  .save()
+
+# COMMAND ----------
+
+server = "factoredata2024.database.windows.net"
+db = "dactoredata2024"
+
+# JDBC connection properties
+jdbc_url = f"jdbc:sqlserver://factoredata2024.database.windows.net:1433;database=dactoredata2024;user=factoredata2024admin@factoredata2024;password=mdjdmliipo3^%^$5mkkm63;encrypt=true;trustServerCertificate=false;hostNameInCertificate=*.database.windows.net;loginTimeout=30;"
+
+connection_properties = {
+    "user": f"factoredata2024admin@{server}",
+    "password": "mdjdmliipo3^%^$5mkkm63",
+    "driver": "com.microsoft.sqlserver.jdbc.SQLServerDriver"
+}
+
+# Table name in Azure SQL Database
+table_name = "gkg.THEMES"
+
+# Write DataFrame to Azure SQL Database
+df_sorted.write.jdbc(url=jdbc_url, table=table_name, mode="overwrite", properties=connection_properties)
+
+# COMMAND ----------
+
 # HERE WE WRITE ALL THE THEMES TO THE GOLD LAYER TO THEN PROCESS THEM USING NLP TO TAG EACH NEW
-storage_account_key = "wgbe0Fzs4W3dPNc35dp//uumz+SPDXVLLGu0mNaxTs2VLHCCPnD7u79PYt4mKeSFboqMRnZ+s+ez+ASty+k+sQ=="
-storage_account_name = "factoredatathon2024"
-container_name = "gold"
+# storage_account_key = "wgbe0Fzs4W3dPNc35dp//uumz+SPDXVLLGu0mNaxTs2VLHCCPnD7u79PYt4mKeSFboqMRnZ+s+ez+ASty+k+sQ=="
+# storage_account_name = "factoredatathon2024"
+# container_name = "gold"
 
-spark.conf.set(
-    f"fs.azure.account.key.{storage_account_name}.blob.core.windows.net",
-    f"{storage_account_key}"
-)
+# spark.conf.set(
+#     f"fs.azure.account.key.{storage_account_name}.blob.core.windows.net",
+#     f"{storage_account_key}"
+# )
 
-file_path = f"wasbs://{container_name}@{storage_account_name}.blob.core.windows.net/gkg/themesSortedGold.csv"
-# weightedAvgGoldsteinToneGold = df_sorted.coalesce(1)
-df_sorted.coalesce(1).write.format("csv").mode("overwrite").option("mergeSchema", "true").option('header', 'true').save(file_path)
+# file_path = f"wasbs://{container_name}@{storage_account_name}.blob.core.windows.net/gkg/themesSortedGold.csv"
+# # weightedAvgGoldsteinToneGold = df_sorted.coalesce(1)
+# df_sorted.coalesce(1).write.format("csv").mode("overwrite").option("mergeSchema", "true").option('header', 'true').save(file_path)
 
 # COMMAND ----------
 
--- databricks/gkg/gkg_readThemesGold.py
+++ databricks/gkg/gkg_readThemesGold.py
@@ -1,4 +1,9 @@
 # Databricks notebook source
+import numpy as np
+import pandas as pd
+
+# COMMAND ----------
+
 storage_account_key = "wgbe0Fzs4W3dPNc35dp//uumz+SPDXVLLGu0mNaxTs2VLHCCPnD7u79PYt4mKeSFboqMRnZ+s+ez+ASty+k+sQ=="
 storage_account_name = "factoredatathon2024"
 container_name = "gold"
@@ -10,10 +15,52 @@ spark.conf.set(
 
 file_path = f"wasbs://{container_name}@{storage_account_name}.blob.core.windows.net/gkg/themesSortedGold.csv"
 df = spark.read.format("csv").option("header", "true").load(file_path)
+df = df.dropna(subset=["THEMES_EXPLODED"])
 
 # COMMAND ----------
 
+themes = df.toPandas()
+themes_list = list(themes['THEMES_EXPLODED'])
 
+# COMMAND ----------
+
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.cluster import KMeans
+from sklearn.preprocessing import StandardScaler
+
+# Ejemplo básico de clustering
+vectorizer = TfidfVectorizer(stop_words='english', max_features=5000)
+X = vectorizer.fit_transform(themes_list)  # themes_list es la lista de temas
+
+scaler = StandardScaler()
+# X_scaled = scaler.fit_transform(X)  # X es tu matriz TF-IDF o embeddings
+
+# kmeans = KMeans(n_clusters=10, random_state=42)
+# kmeans.fit(X_scaled)
+
+kmeans = KMeans(n_clusters=10, init='k-means++', max_iter=500, n_init=10, random_state=42)
+kmeans.fit(X)
+
+# kmeans = KMeans(n_clusters=10, random_state=42)  # Ajusta el número de clusters según necesites
+# kmeans.fit(X)
+
+# Agrega el cluster resultante al DataFrame
+themes['Cluster'] = kmeans.labels_
+
+# COMMAND ----------
+
+from sklearn.manifold import TSNE
+import matplotlib.pyplot as plt
+
+tsne = TSNE(n_components=2, random_state=42)
+X_tsne = tsne.fit_transform(X)
+
+plt.scatter(X_tsne[:, 0], X_tsne[:, 1], c=kmeans.labels_, cmap='viridis')
+plt.show()
+
+# COMMAND ----------
+
+len(themes.query('Cluster == 0'))
 
 # COMMAND ----------
 
