97102b23b2207e597f3c511e60875cc7a0764d89
Cesar Arroyo <carroyo@cantilever.com.mx>
2024-08-19 14:14:01 -0600

    add: Modularization for the taipy pages Presentation page. Dashboard page.

--- web_solution/web_app.py
+++ web_solution/web_app.py
@@ -1,56 +1,11 @@
-import plotly.graph_objects as go
-import taipy.gui.builder as tgb
-from library.processing import process_df_to_plot
+from library.dashboard_page import dashboard_page
+from library.presentation_page import presentation_page
 from taipy.gui import Gui
 
-datasets_dict = process_df_to_plot()
-
-# ts_plot_config = {
-#     "name_df": datasets_dict.keys(),
-#     "names": datasets_dict.keys().mapping(lambda x: x.capitalize()),
-#     "colors": ["blue", "orange", "green"],
-# }
+root_md = "<|navbar|>"
 
 
-def create_time_series_plot():
-    # Extract datasets
-    train_df = datasets_dict["train"]
-    test_df = datasets_dict["test"]
-    real_df = datasets_dict["real"]
-
-    trace_train = go.Scatter(
-        x=train_df["date"],
-        y=train_df["goldstein"],
-        mode="lines",
-        name="Train",
-        line=dict(color="blue"),
-    )
-
-    trace_test = go.Scatter(
-        x=test_df["date"],
-        y=test_df["goldstein"],
-        mode="lines",
-        name="Test",
-        line=dict(color="orange"),
-    )
-
-    trace_real = go.Scatter(
-        x=real_df["date"],
-        y=real_df["goldstein"],
-        mode="lines",
-        name="Real",
-        line=dict(color="green"),
-    )
-
-    fig = go.Figure(data=[trace_train, trace_test, trace_real])
-
-    return fig
+pages = {"/": root_md, "Presentation": presentation_page, "Dashboard": dashboard_page}
 
 
-fig = create_time_series_plot()
-
-with tgb.Page() as page:
-    tgb.text("# Latam Fusion Dashboard", mode="md")
-    tgb.chart(figure="{fig}")
-
-Gui(page).run(host="0.0.0.0", port=5000)
+Gui(pages=pages).run(host="0.0.0.0", port=5000, use_reloader=True)
