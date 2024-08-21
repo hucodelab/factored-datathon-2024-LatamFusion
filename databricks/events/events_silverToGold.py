06aa40bb2bce0042e0cccdf98c961a823e9268da
hucodelab <hucodelab>
2024-08-21 04:03:31 +0000

    update: orginize notebooks nomenclatures and begin reading themes labeled

--- databricks/events/events_silverToGold.py
+++ databricks/events/events_silverToGold.py
@@ -13,20 +13,25 @@ from pyspark.sql import functions as F
 
 # COMMAND ----------
 
-delta_path = "/mnt/bronze/eventsBronze"
+delta_path = "/mnt/silver/eventsSilver"
 # Verify by reading the Delta table back (optional)
-eventsBronze = spark.read.format("delta").load(delta_path)
+eventsSilver = spark.read.format("delta").load(delta_path)
 
 # COMMAND ----------
 
 # filtered by columns
 delta_path = "/mnt/silver/eventsDAGASilver1"
 
-eventsDAGASilver1 = eventsBronze \
+eventsDAGASilver1 = eventsSilver \
     .select('DATE','Actor1CountryCode','ActionGeo_CountryCode','NumMentions','GoldsteinScale','AvgTone') \
         .na.drop(subset=['ActionGeo_CountryCode','GoldsteinScale','AvgTone','NumMentions'])
 
 eventsDAGASilver1.write.format("delta").mode("overwrite").option("mergeSchema", "true").save(delta_path)
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ### Read from silver layer
 
 # COMMAND ----------
 
@@ -36,19 +41,11 @@ spark.read.format("delta").load(delta_path).show()
 
 # COMMAND ----------
 
-from pyspark.sql import functions as F
-
 # Group the DataFrame by 'group_column'
 weightedAvgGoldsteinToneGold = eventsDAGASilver1.groupBy("DATE","ActionGeo_CountryCode").agg(
     (F.sum(F.col("GoldsteinScale") * F.col("NumMentions")) / F.sum("NumMentions")).alias("GoldsteinScaleWA"),
     (F.sum(F.col("AvgTone") * F.col("NumMentions")) / F.sum("NumMentions")).alias("AvgToneWA")
 )
-
-# delta_path = "/mnt/gold/weightedAvgGoldsteinToneGold"
-# weightedAvgGoldsteinToneGold.write.format("delta").mode("overwrite").option("mergeSchema", "true").save(delta_path)
-
-# # Show the result
-# weighted_avg_df.show()
 
 # COMMAND ----------
 
@@ -68,4 +65,4 @@ weightedAvgGoldsteinToneGold.write.format("csv").mode("overwrite").option("merge
 # COMMAND ----------
 
 # print(df_read.count())
-# 46 millions of rows !!!
+# 46 millions of rows
