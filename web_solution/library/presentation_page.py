c80be56e70c96ade19e9c5671b3d1197ce011cf6
Cesar Arroyo <carroyo@cantilever.com.mx>
2024-08-21 13:56:07 -0600

    style: Markdown is now within the presentation_page.py file md file is still kept for ease of development.

--- web_solution/library/presentation_page.py
+++ web_solution/library/presentation_page.py
@@ -5,6 +5,30 @@ import os
 # Get path of the current file
 path = os.path.abspath(__file__)
 
-# Read file presentation_page.md from the same directory
-with open(os.path.join(os.path.dirname(path), "presentation_page.md")) as f:
-    presentation_page = f.read()
+presentation_page = """
+# Factored Datathon 2024 - The GDELT Challenge
+
+Check the next sections to understand how we analyzed the GDELT dataset to create a risk assessment dashboard.
+
+# Overview
+
+# Architecture
+
+# Machine Learning
+
+# Dashboard
+
+# About us
+
+Our team is composed of 4 members with different backgrounds and experiences. We are all passionate about data science and we are excited to share our findings with you. The name *LatamFusion* comes from the fact that we are all from different countries in Latin America and we are fusing our knowledge to create a great solution.
+
+The team members are:
+
+- [Hugo Vallejo](https://www.linkedin.com/in/hugo-r-vallejo-angulo/): Based on São Paulo, Brasil and originally from Caracas, Venezuela. Hugo is a PhD candidate in Artificial Intelligence at Universidade de São Paulo. Currently working as Data Engineer, Hugo contributed to the project by setting up the data pipeline and the ML model.
+
+- [Agustín Rodríguez](https://www.linkedin.com/in/agustinnrodriguez/): Based on Buenos Aires, Argentina, and originally from the same city. Agustín is a Data & AI Enthusiast. Currently working as Backend Developer, he contributed to the project by defining the business goal, exploring the data and proposing analytics and ML solutions.
+
+- [Jesús Castillo](https://www.linkedin.com/in/jes%C3%BAs-castillo/): Based on La Serena, Chile, and originally from the same city. Jesús is a Data Scientist. He comes from a background as a translator and interpreter. He is currently looking forward to expand his knowledge in the field of data science, particularly in LLMs.
+
+- [César Arroyo](https://www.linkedin.com/in/cesar-arroyo-cardenas): Based on Ciudad de México, México, and originally from Cartagena, Colombia. César has worked as Data Scientist and BI Developer. Currently working as Data Scientist, he contributed to the project by automating processes and setting up the web application.
+"""
