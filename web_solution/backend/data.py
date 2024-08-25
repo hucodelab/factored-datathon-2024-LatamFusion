63d5fdbdfb5aa0250932784f04b70af27e5714c9
Cesar Arroyo <carroyo@cantilever.com.mx>
2024-08-24 18:33:08 -0600

    viz: Plot real data

--- web_solution/backend/data.py
+++ web_solution/backend/data.py
@@ -14,6 +14,14 @@ FROM dactoredata2024.events.goldsteinPredictionsGold
 
 try:
     goldstein_data_df = connection.fetch_data_as_dataframe(goldstein_prediction_gold)
+
 except (NoDBConnection, Exception):
     goldstein_data_df = None
     print("No connection to the database.")
+
+
+def get_goldstein_country(country: str):
+    if goldstein_data_df is None:
+        return None
+
+    return goldstein_data_df[goldstein_data_df["pais"] == country].copy()
