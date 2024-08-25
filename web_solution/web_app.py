7967b8a2987c56bcb9f24e0fc67f5b353a894cfd
Cesar Arroyo <carroyo@cantilever.com.mx>
2024-08-24 20:37:34 -0600

    refactor: Moved root code to pages

--- web_solution/web_app.py
+++ web_solution/web_app.py
@@ -1,14 +1,9 @@
-from dotenv import load_dotenv
 from pages.dashboard_page import dashboard_page
 from pages.presentation_page import presentation_page
+from pages.root import root_md
 from taipy.gui import Gui
-
-# load_dotenv(".env")
-
-root_md = "<|navbar|>"
-
 
 pages = {"/": root_md, "Presentation": presentation_page, "Dashboard": dashboard_page}
 
 
-Gui(pages=pages).run(host="0.0.0.0", port=5000, use_reloader=True)
+Gui(pages=pages).run(host="0.0.0.0", port=5000, debug=True, title="GDELT Dashboard")
